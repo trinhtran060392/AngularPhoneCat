203e3ed8ace71bbb8090a10e41f337a2d62df1c6
TrinhTV3 <trinhtv060392@gmail.com>
2014-12-18 14:38:47 +0700

    test app overview

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,15 +4,22 @@
 describe('PhoneCat controllers', function(){
 
 	describe('PhoneListCtrl', function() {
-
+		var scope, ctrl, $httpBackend;
 	  beforeEach(module('phonecatApp'));
 
-	  it('should create "phones" model with 3 phones', inject(function($controller){
-
-	  	var scope = {},
-	  		ctrl = $controller('PhoneListCtrl',{$scope:scope});
-	  		expect(scope.phones.length).toBe(3);
-	  }));
-
+	 	beforeEach(inject(function(_$httpBackend_,$rootScope, $controller) {
+	 		$httpBackend = _$httpBackend_;
+	 		$httpBackend.expectGET('phones/phones.json').respond([{name: 'Nexus S'},{name: 'Motorola DROID'}]);
+	 		scope = $rootScope.$new();
+	 		ctrl = $controller('PhoneListCtrl',{$scope: scope});
+	 	}));
+	 	it('should create "phones" model with 2 phones fetched from xhr', function() {
+	 		expect(scope.phones).toBeUndefined();
+	 		$httpBackend.flush();
+	 		expect(scope.phones).toEqual([{name: 'Nexus S'},{name : 'Motorola DROID'}]);
+	 	});
+	 	it('should set the default value of orderProp model', function() {
+	 		expect(scope.orderProp).toBe('age');
+	 	});
 	});
 });

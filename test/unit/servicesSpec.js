81e80153fe3f7413e782652c67eea290f81a7134
TrinhTV3 <trinhtv060392@gmail.com>
2014-12-19 15:54:46 +0700

    check if Phone service exist

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -3,5 +3,8 @@
 /* jasmine specs for services go here */
 
 describe('service', function() {
-
+	beforeEach(module('phonecatApp'));
+	it('check the exitence of Phone factory', inject(function(Phone)){
+		expect(Phone).toBeDefined();
+	});
 });

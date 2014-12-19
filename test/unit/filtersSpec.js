2c24a0684ebe0f66f0becf3c40a0c5f51ce9b77e
TrinhTV3 <trinhtv060392@gmail.com>
2014-12-19 15:53:26 +0700

    test for filter

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -4,4 +4,12 @@
 
 describe('filter', function() {
 	beforeEach(module('phonecatFilters'));
+	describe('checkmark', function(){
+		it('should convert boolean values to unicode checkmark or cross',
+			inject(function(checkmarkFilter){
+				expect(checkmarkFilter(true));
+			})
+			);
+
+	});
 });

7b303c8f09b708e2baf89e1b7efdf21343e1ae25
TrinhTV3 <trinhtv060392@gmail.com>
2014-12-19 15:49:57 +0700

    using customize service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,19 +2,20 @@
 
 /* Controllers */
 var phonecatControllers = angular.module('phonecatControllers',[]);
-phonecatControllers.controller('PhoneListCtrl',['$scope', '$http', function($scope,$http){
+phonecatControllers.controller('PhoneListCtrl',['$scope', 'Phone', function($scope,Phone){
 
-	$http.get('phones/phones.json').success(function(data){
-
-		$scope.phones = data;
-	});
+	$scope.phones= Phone.query();
 	$scope.orderProp = 'age';
 }]);
-phonecatControllers.controller('PhoneDetailCtrl',['$scope','$routeParams','$http',
+phonecatControllers.controller('PhoneDetailCtrl',['$scope','$routeParams','Phone',
 
-	function($scope,$routeParams,$http){
-		$http.get('phones/'+$routeParams.phoneId+'.json').success(function(data){
+	function($scope,$routeParams,Phone){
+		
+		$scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone){
+			$scope.mainImageUrl = phone.images[0];
 
-			$scope.phone = data;
 		});
+		$scope.setImage = function(imageUrl){
+			$scope.mainImageUrl = imageUrl;
+		}
 	}]);
